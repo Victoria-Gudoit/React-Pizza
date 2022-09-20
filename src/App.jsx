d5ed9ddbf4eac4e8ae764b819e38b435c3b903df
Victoria-Gudoit <vgudojt@mail.ru>
2022-09-20 18:16:03 +0300

    add PizzaBlock and Sort.jsx

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./scss/app.scss";
+import { Header } from "components/Header";
+import { Categories } from "./components/Categories";
+import { Sort } from "./components/Sort";
+import { PizzaBlock } from "./components/PizzaBlock";
+import { useState, useEffect } from "react";
+
+export const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://6328dc1dd2c97d8c525e3c1f.mockapi.io/items")
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  });
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
