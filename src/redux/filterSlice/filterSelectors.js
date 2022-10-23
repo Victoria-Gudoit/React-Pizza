d8b7d09711b3102afa7eb3b278c09c97cb84953e
Victoria-Gudoit <vgudojt@mail.ru>
2022-10-23 19:35:16 +0300

    built projects

--- src/redux/filterSlice/filterSelectors.js
+++
@@ -1,5 +0,0 @@
-const getFilterSlice = (state) => state.filter;
-
-export const getCategoryId = (state) => getFilterSlice(state).categoryId;
-
-export const getSortType = (state) => getFilterSlice(state).sort;
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { filterReducer } from "./filterSlice";
-
-const rootReducer = combineReducers({
-  filter: filterReducer,
-});
-export const store = configureStore({
-  reducer: rootReducer,
-});
